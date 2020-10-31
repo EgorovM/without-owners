d5de61ba7274d3e4dc77cf5ae27e8ebe1598d526
Soulusside <70377494+Soulusside@users.noreply.github.com>
2020-10-31 17:04:07 +0900

    Add files via upload
    edit models

--- shelter/models.py
+++ shelter/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Shelter(models.Model):
+
+    shelter_address = models.CharField(max_length=126)
+    company = models.CharField(max_length=126)
+    leader = models.CharField(max_length=126)
+
+    def __str__(self):
+        return f"{self.company}"
+
+
+class Shelter_staff(models.Model):
+    name = models.CharField(max_length=31)
+
+    def __str__(self):
+        return self.name
